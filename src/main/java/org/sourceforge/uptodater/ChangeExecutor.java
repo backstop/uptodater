32879cee0572daf57697118bb9c0ac1bd85d8129
antifun <antifun@46f30fe5-53ee-4092-a685-b018ee6592cd>
2007-12-29 00:30:25 +0000

    Using PreparedStatements is pointless since we never do an actual query, and causes breakage when something that looks like a bound parameter passes by (at least in Oracle it does).  So I changed it to use normal Statements for those update cases.  (Callable is not affected.)
    Bumped rev to 0.9.1.

--- src/main/java/org/sourceforge/uptodater/ChangeExecutor.java
+++ src/main/java/org/sourceforge/uptodater/ChangeExecutor.java
@@ -4,6 +4,11 @@ import java.sql.*;
 
 public abstract class ChangeExecutor {
 
+    public static interface DeferredStatement {
+        void doIt() throws SQLException;
+        Statement getStatement();
+    }
+
     protected String sqlText;
     protected Connection connection;
 
@@ -13,17 +18,17 @@ public abstract class ChangeExecutor {
 
     public final void execute(Connection con) throws SQLException {
         this.connection = con;
-        PreparedStatement preparedStatement = null;
+        DeferredStatement deferredStatement = null;
         try {
-            preparedStatement = createStatement();
-            preparedStatement.executeUpdate();
+            deferredStatement = createDeferredStatement();
+            deferredStatement.doIt();
         } finally {
-            DBUtil.close(preparedStatement);
+            DBUtil.close(deferredStatement.getStatement());
         }
     }
 
 
-    abstract PreparedStatement createStatement() throws SQLException;
+    abstract DeferredStatement createDeferredStatement() throws SQLException;
 
 
     public static ChangeExecutor createChangeExecutor(final String sqlText) {
@@ -36,21 +41,45 @@ public abstract class ChangeExecutor {
     }
 
     public static class CallableChangeExecutor extends ChangeExecutor {
+        CallableStatement stmt;
 
         private CallableChangeExecutor(String sqlText) {
             super(sqlText);
         }
-        public PreparedStatement createStatement() throws SQLException {
-            return connection.prepareCall(sqlText);
+        public DeferredStatement createDeferredStatement() throws SQLException {
+            stmt = connection.prepareCall(sqlText);
+            return new DeferredStatement() {
+
+                public void doIt() throws SQLException {
+                    stmt.execute();
+                }
+
+                public Statement getStatement() {
+                    return stmt;
+                }
+            };
         }
     }
 
     public static class StatementChangeExecutor extends ChangeExecutor {
+        Statement stmt;
+
         private StatementChangeExecutor (String sqlText) {
             super(sqlText);
         }
-        public PreparedStatement createStatement() throws SQLException {
-            return connection.prepareStatement(sqlText);
+
+        public DeferredStatement createDeferredStatement() throws SQLException {
+            stmt = connection.createStatement();
+            return new DeferredStatement() {
+
+                public void doIt() throws SQLException {
+                    stmt.executeUpdate(sqlText);
+                }
+
+                public Statement getStatement() {
+                    return stmt;
+                }
+            };
         }
     }
 }
