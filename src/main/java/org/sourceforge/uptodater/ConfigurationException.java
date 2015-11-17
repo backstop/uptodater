5c1304e7094a7a33f22bd2da176e132a4ab37528
Brian Riehman <brian@riehman.com>
2015-11-17 15:54:41 -0600

    Convert commons-logging to slf4j
    Replace Java Commons Logging with slf4j to make sure we are independent
    in a good way.

--- src/main/java/org/sourceforge/uptodater/ConfigurationException.java
+++ src/main/java/org/sourceforge/uptodater/ConfigurationException.java
@@ -1,12 +1,5 @@
 package org.sourceforge.uptodater;
 
-/* 
- * Created Date: Jan 10, 2005
- */
-
-/**
- * @author rapruitt
- */
 public class ConfigurationException extends RuntimeException {
     public ConfigurationException(String message) {
         super(message);
